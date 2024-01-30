aafd709411b3b16ad4449f75c1dc1eefbc046dba
ling qiao <2676272310@qq.com>
2024-01-30 17:10:53 +0800

    [lingqiao] init the project

---
+++ src/main/java/com/example/twmarsrover/TwMarsroverApplication.java
@@ -0,0 +1,13 @@
+package com.example.twmarsrover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwMarsroverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwMarsroverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/twmarsrover/TwMarsroverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twmarsrover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwMarsroverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
